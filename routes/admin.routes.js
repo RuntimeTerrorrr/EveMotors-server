7ef9fd1cc9c7e1fa21f88a360328f72e686c1237
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 00:26:52 +0500

    Test

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -116,11 +116,10 @@ adminRouter.post('/dashboard', upload.single, async (req, res) => {
             fixedScaleValue,
             fixedTargetValue, 
         });
-
         res.status(201).json({ message: "The Car has been added.", newCar });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: "An error occurred while adding the car." });
+        res.status(500).json({ message: err });
     }
 });
 
