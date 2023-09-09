b1b70811e7fc9cd27e4559b907434751cfba290e
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 23:22:04 +0500

    Chnaged the CORs code

--- app.js
+++ app.js
@@ -9,18 +9,10 @@ import cors from 'cors';
 const port = process.env.PORT || 3000;
 const app = express();
 
-const allowedOrigins = ['https://eve-motors-app.vercel.app'];
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-};
+app.use(cors({
+    origin: [ 'https://www.eve-motors.vercel.app/']
+}));
 
-app.use(cors(corsOptions));
 config();
 connectDB();
 
