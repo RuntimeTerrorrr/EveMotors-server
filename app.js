7d8f0a76ae1778f7b828cf967d50b7352337e57f
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 15:33:33 +0500

    Access control headers added

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ const corsOptions = {
     },
 };
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(cors(corsOptions));
 config();
 connectDB();
