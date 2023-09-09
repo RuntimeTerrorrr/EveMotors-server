f7d69fb964f38c3e50365d7d9c97e35216482fa2
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 23:37:22 +0500

    CORs Origin Updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin: [ 'https://www.eve-motors-app.vercel.app/']
+    origin: [ 'https://eve-motors-app.vercel.app']
 }));
 
 config();
