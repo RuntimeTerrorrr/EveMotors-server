c68c07318ffdc26c93974edd8a3eaef9b4a95107
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-10 00:24:41 +0500

    Multer Added

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ import adminRouter from './routes/admin.routes.js';
 import mainRouter from './routes/main.routes.js';
 import { connectDB } from './config/db.js';
 import cors from 'cors';
+import multer from 'multer';
 
 const port = process.env.PORT || 3000;
 const app = express();
+const upload = multer();
 
+app.use(upload.array());
 app.use(cors({
     origin: [ 'https://eve-motors-app.vercel.app']
 }));
