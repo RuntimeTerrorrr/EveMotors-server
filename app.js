f5686ae05447c6cf9aeef6ce21a73a4902fc2313
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 23:13:34 +0500

    App.js Updated

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import adminRouter from './routes/admin.routes.js';
 import mainRouter from './routes/main.routes.js';
 import { connectDB } from './config/db.js';
 import cors from 'cors';
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 const app = express();
 
 const allowedOrigins = ['https://eve-motors-app.vercel.app'];
@@ -23,16 +24,13 @@ app.use(cors(corsOptions));
 config();
 connectDB();
 
-
 app.use(bodyParser.json());
-app.use(cors());
 
 app.use('/admin', adminRouter)
 app.use('/main', mainRouter)
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
 });
-
 
 export default app;
