4d2382c00c3567b302704fb9cff4aaf1c5e19c65
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 14:58:18 +0500

    cors Origin updated

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ const app = express();
 
 const upload = multer;
 
-const allowedOrigins = ['http://localhost:5173'];
 const corsOptions = {
     origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        if (origin === '*' || allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
