9340065af2848ffffdcca7caf1602cc155f96880
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-09-09 13:06:18 +0500

    Header removed

--- app.js
+++ app.js
@@ -23,14 +23,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 config();
 connectDB();
 
